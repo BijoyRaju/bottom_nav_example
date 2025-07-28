040c0f409ca9b28e229b4f66da4bd23a22676728
Bijoy Raju <91817954+BijoyRaju@users.noreply.github.com>
2025-07-28 20:52:48 +0530

    Update Search.dart

--- lib/Search.dart
+++ lib/Search.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-
 class Search extends StatelessWidget {
   const Search({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
